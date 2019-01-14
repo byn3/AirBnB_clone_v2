89e51bd5dcd62f3ee05ca96bcdd8806c9cc7a8d2
byn3 <bryanleung92@gmail.com>
2019-01-14 23:47:40 +0000

    Add: script that prepares web servers with default folders and settings

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Sets up the environment and directory for web server deployment
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html><head>poooop</head>Poop.</html>" | sudo tee -a /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chmod -R u+g /data/
+sudo sed -i "29i location /hbnb_static{\nalias /data/web_static/current/;\n}" /etc/nginx/sites-available/default
+sudo service nginx restart
