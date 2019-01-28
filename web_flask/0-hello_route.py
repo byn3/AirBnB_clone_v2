dfcbcf25027d9385e8cb4e43871afb3b3f2aa002
Bryan Leung <bryanleung92@gmail.com>
2019-01-28 15:17:38 -0800

    Change: Added pep8 styling

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ Write a script that starts a Flask web application:
 
 from flask import Flask
 app = Flask(__name__)
- 
+app.url_map.strict_slashes = False
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
- 
+    return "Hello HBNB!"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
