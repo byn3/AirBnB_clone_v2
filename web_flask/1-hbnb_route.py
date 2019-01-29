364a652c1601f90c4d2f670d3aa2c24e2d519c28
byn3 <bryanleung92@gmail.com>
2019-01-29 21:26:04 +0000

    Add: reverted changes and removed ! in string

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,7 +5,7 @@ Write a script that starts a Flask web application:
 
 from flask import Flask
 app = Flask(__name__)
-# app.url_map.strict_slashes = False
+app.url_map.strict_slashes = False
 
 
 @app.route("/")
@@ -14,10 +14,10 @@ def hello():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hello1():
     """ hbnb extension """
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
